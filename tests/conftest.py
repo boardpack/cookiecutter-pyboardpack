7f77e586b573884b06c78efe315134e5c7cb4571
dukkee <urchin.dukkee@gmail.com>
2022-11-27 16:48:01 +0100

    Fix yaml loading

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,6 @@ import yaml
 @pytest.fixture
 def base_context():
     with open("./tests/default_context.yml") as fp:
-        data = yaml.load(fp)
+        data = yaml.load(fp, Loader=yaml.FullLoader)
 
     return data["default_context"]
